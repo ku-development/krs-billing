6fbd377836496b778dbcc34456b52e8dfebb4da9
KurdY <liadpeleg03@gmail.com>
2024-08-09 13:19:23 +0300

    created new config

---
+++ Shared/Sh_Config.lua
@@ -0,0 +1,38 @@
+Config = {}
+
+Config.Jobs = {
+    ['police'] = {
+        ['1'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+        ['2'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+        ['3'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+    },
+    ['ambulance'] = {
+        ['1'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+        ['2'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+        ['3'] = {
+            CanBill = true, -- can this grade bill players?
+            BossAccess = false, -- can access the refund page?
+            InspectCitizen = true, -- can this grade access this page?
+        },
+    },
+}
---
+++ Shared/Sv_Config.lua
@@ -0,0 +1,4 @@
+Config.Webhooks = {
+    SendBill = '', -- webhook for send bill
+    RefundBill = '', -- webhook for Refunt bill
+}
