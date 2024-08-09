28d85d37602ed06a801a22fa2c44882a5d08f436
KurdY <liadpeleg03@gmail.com>
2024-08-09 16:29:27 +0300

    new function to get bills for job

--- Server/Server.lua
+++ Server/Server.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: undefined-global
+
 function GetPlayerBills(cid)
     local unpaidBills = {}
     local billingHistory = {}
@@ -37,4 +39,40 @@ function GetPlayerBills(cid)
     end
 
     return unpaidBills, billingHistory
+end
+
+function GetSocietyBills(job)
+    local societyBills = {}
+    local billResults = MySQL.query.await("SELECT * FROM `bills` WHERE sender_job = @sender_job", {['@sender_job'] = job})
+
+    for _, bill in pairs(billResults) do
+        local receiverData = MySQL.query.await("SELECT charinfo, job FROM `players` WHERE citizenid = @receiver_cid", {['@receiver_cid'] = bill.receiver_cid})
+        local senderData = MySQL.query.await("SELECT charinfo FROM `players` WHERE citizenid = @sender_cid", {['@sender_cid'] = bill.sender_cid})
+
+        local receiverCharinfo = json.decode(receiverData[1].charinfo)
+        local receiverJob = json.decode(receiverData[1].job)
+
+        local senderCharinfo = json.decode(senderData[1].charinfo)
+
+        local receiverName = receiverCharinfo.firstname .. " " .. receiverCharinfo.lastname
+        local senderName = senderCharinfo.firstname .. " " .. senderCharinfo.lastname
+
+        local billData = {
+            id = bill.id,
+            amount = bill.amount,
+            reason = bill.reason,
+            sender = senderName,
+            receiver = receiverName,
+            job = receiverJob.name,
+            date = bill.date,
+            time = bill.time,
+            paid = bill.paid,
+            sender_cid = bill.sender_cid,
+            receiver_cid = bill.receiver_cid
+        }
+
+        table.insert(societyBills, billData)
+    end
+
+    return societyBills
 end
